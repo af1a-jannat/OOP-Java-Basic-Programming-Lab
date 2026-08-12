4ad027335e3100ff172a899f10b8d17d037a0c5f
af1a-jannat <afiajannat301006@gmail.com>
2026-08-12 21:58:30 +0600

    Add files via upload

---
+++ OOP Java Basic Programming Lab 2/Average.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Average {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double a = input.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double b = input.nextDouble();
+
+        System.out.print("Enter third number: ");
+        double c = input.nextDouble();
+
+        double average = (a + b + c) / 3;
+
+        System.out.println("Average = " + average);
+    }
+}
