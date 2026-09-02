3985009cc29f25bd31fccc524b21da616808dd1a
af1a-jannat <afiajannat301006@gmail.com>
2026-09-02 20:06:50 +0600

    Create Person.java

---
+++ OOP Java Programming Lab 5/Person.java
@@ -0,0 +1,26 @@
+lass Person {
+
+    String name;
+    int age;
+
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public static void main(String[] args) {
+
+        Person person1 = new Person("Alice", 25);
+        Person person2 = new Person("Bob", 30);
+
+        System.out.println("Person 1:");
+        System.out.println("Name: " + person1.name);
+        System.out.println("Age: " + person1.age);
+
+        System.out.println();
+
+        System.out.println("Person 2:");
+        System.out.println("Name: " + person2.name);
+        System.out.println("Age: " + person2.age);
+    }
+}
